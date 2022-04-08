d94274419ea665adf4e7372b7f97ed36ecbab23b
Biraj <birajpathak10@gmail.com>
2022-04-08 19:19:04 +0545

    home page and pain page started

--- apma/lib/screens/home_screen.dart
+++ apma/lib/screens/home_screen.dart
@@ -11,6 +11,33 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Text('Homescreen'),
       ),
