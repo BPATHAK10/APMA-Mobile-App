05c66ea214aa76ca53e9bc9d247a9fad24e5dcb2
Biraj <birajpathak10@gmail.com>
2022-04-08 22:09:39 +0545

    homepage nearly finished

--- apma/lib/screens/home_screen.dart
+++ apma/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:apma/widgets/custom_tile.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -11,36 +12,84 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Home'),
         ),
-      ),
-      body: Center(
-        child: Text('Homescreen'),
-      ),
-    );
+        bottomNavigationBar:
+            BottomNavigationBar(
+              items:  <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Image.asset('assets/icons/reports.png', width: 30, height: 30),
+                  label: "Reports",
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset('assets/icons/resources.png', width: 30, height: 30,),
+                  label: "Resources"
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset('assets/icons/journal.png', width: 30, height: 30),
+                  label: "Journal"
+                ),
+              ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              const DrawerHeader(
+                child: const Text('Drawer Header'),
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: GridView.count(
+          padding: const EdgeInsets.all(25),
+          crossAxisCount: 2,
+          mainAxisSpacing: 15,
+          crossAxisSpacing: 15,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: <Widget>[
+            const MyTile(
+              title: "Pain",
+            ),
+            const MyTile(
+              title: "Energy Level",
+            ),
+            const MyTile(
+              title: "Exercise",
+            ),
+            const MyTile(
+              title: "Sleep",
+            ),
+            const MyTile(title: "Work"),
+            const MyTile(title: "Toolkit"),
+            // Container(
+            //   child: Text("item 1"),
+            // ),
+            //    Container(
+            //   child: Text("item 2"),
+            // ),
+            // Container(
+            //   child: Text("item 3"),
+            // ),
+            // Container(
+            //   child: Text("item 4"),
+            // ),
+          ],
+        ));
   }
 }
