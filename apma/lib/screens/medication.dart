e4cfbb1e40601b9721648f53e758048eae88a514
Aagat <aagatpokhrel@gmail.com>
2022-04-20 11:33:45 +0545

    medication page added

--- apma/lib/screens/medication.dart
+++ apma/lib/screens/medication.dart
@@ -3,7 +3,6 @@ import 'package:apma/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-
 class MedicTrack extends StatefulWidget {
   MedicTrack({Key? key}) : super(key: key);
 
@@ -17,6 +16,67 @@ class _MedicTrackState extends State<MedicTrack> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: showAppBar(context,'Medications'),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: (){
+          showDialog(
+            context: context,
+            builder: (BuildContext context){
+              return AlertDialog(
+                scrollable: true,
+                title: const Text('Login'),
+                content: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                    child: Column(
+                      children: <Widget>[
+                        // TableCalendar(
+                        //   calendarFormat : CalendarFormat.week,
+                        //   firstDay: DateTime.utc(2020, 10, 16),
+                        //   lastDay: DateTime.utc(2030, 3, 14),
+                        //   focusedDay: DateTime.now(),
+                        // ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Name of Medicine',
+                            icon: Icon(Icons.account_box),
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'User Dosage',
+                            icon: Icon(Icons.email),
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Frequency',
+                            icon: Icon(Icons.email),
+                          ),
+                        ),
+                      ]
+                    ),
+                  ),
+                ),
+                actions: [
+                  ElevatedButton(
+                    child: const Text("Submit"),
+                    onPressed: () {
+                      
+                    }
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+
+                    },
+                    child: const Text("Cancel")
+                  )
+                ],
+              );
+            }
+          );
+        }
+      ),
       body: Container(
         child: Column(
           children: [
