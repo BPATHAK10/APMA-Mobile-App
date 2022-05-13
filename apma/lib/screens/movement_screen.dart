fe7d6d69d2386ff617c242340ca2ee53336c7223
Aagat <aagatpokhrel@gmail.com>
2022-05-13 12:47:27 +0545

    final week6

--- apma/lib/screens/movement_screen.dart
+++ apma/lib/screens/movement_screen.dart
@@ -24,7 +24,7 @@ class _ExerciseTrackState extends State<ExerciseTrack> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: showAppBar(context,'Exercise'),
+      appBar: showAppBar(context,'Movement'),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsetsDirectional.fromSTEB(5, 10, 5, 0),
